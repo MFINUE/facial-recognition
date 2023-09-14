805008c7e2e4b01c6819b294c332e9b7a8368eb7
Bora <canboraciner@gmail.com>
2023-09-14 21:25:49 +0300

    Fix formatting and add proper convention

--- Errors.py
+++
@@ -1,7 +0,0 @@
-class PathDoesNotExistError(Exception):
-
-    def __init__(self, path) -> None:
-        self.path = path
-        self.message = f"\"{path}\" is not a valid path."
-        super().__init__(self.message)
-    
